8c397e24f5d781bdb9fe0c2457037fbab3f8ed2e
Gualberto <m4ur0@live.com>
2017-10-04 03:54:50 +0000

    First commit

--- PCA9685.cpp
+++ PCA9685.cpp
@@ -1,105 +1,55 @@
-/*
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- *
- * Name        : PCA9685.cpp
- * Author      : Georgi Todorov
- * Version     :
- * Created on  : Dec 9, 2012
- *
- * Copyright © 2012 Georgi Todorov  <terahz@geodar.com>
- */
-
-#include <sys/stat.h>
-#include <sys/ioctl.h>
+#include <iostream>
+#include <wiringPiI2C.h>
 #include <unistd.h>
-#include <linux/i2c.h>
-#include <linux/i2c-dev.h>
-#include <stdio.h>      /* Standard I/O functions */
-#include <fcntl.h>
-#include <syslog.h>		/* Syslog functionallity */
-#include <inttypes.h>
-#include <errno.h>
-#include <math.h>
-#include <linux/types.h>
-
+#include <stdint.h>
 #include "PCA9685.h"
 
-//! Constructor takes bus and address arguments
-/*!
- \param bus the bus to use in /dev/i2c-%d.
- \param address the device address on bus
- */
-PCA9685::PCA9685(int bus, int address) {
-	i2c = new I2C(bus,address);
-	reset();
-	setPWMFreq(1000);
+using namespace std;
+
+int position[9];
+
+//Sets up the frequency to 60Hz.
+void PCA9685::Set_Freq(int fd){
+  uint8_t prescale_val = (25000000/4096/60)-1;
+  wiringPiI2CWriteReg8(fd, 0x00, 0x10);          //register mode1, Low power mode Turn off Oscillator
+  wiringPiI2CWriteReg8(fd, 0xFE, prescale_val);  //Store the prescale_value(Frequency) into the Prescale register. 
+  wiringPiI2CWriteReg8(fd, 0x00, 0x00);
+  usleep(500);
+  wiringPiI2CWriteReg8(fd, 0x00, 0x80);
+  wiringPiI2CWriteReg8(fd, 0x01, 0x04);
+//  cout<<"Setting up Freq.... Done" <<endl;
 }
 
-PCA9685::~PCA9685() {
-	delete i2c;
-}
-//! Sets PCA9685 mode to 00
-void PCA9685::reset() {
+//Sets servos position using an array. 
+void PCA9685::Set_Pwm(int fd, int degree[]){
+//  cout<<"Start: "<<endl;
+  for(int a = 0; a <= 9; a++)      //converts degrees to servo position.
+  {
+    position[a]= degree[a]*2.5;
+    //cout<<position[a]<<endl;
+  }
 
-		i2c->write_byte(MODE1, 0x00); //Normal mode
-		i2c->write_byte(MODE2, 0x04); //totem pole
-
-}
-//! Set the frequency of PWM
-/*!
- \param freq desired frequency. 40Hz to 1000Hz using internal 25MHz oscillator.
- */
-void PCA9685::setPWMFreq(int freq) {
-
-		uint8_t prescale_val = (CLOCK_FREQ / 4096 / freq)  - 1;
-		i2c->write_byte(MODE1, 0x10); //sleep
-		i2c->write_byte(PRE_SCALE, prescale_val); // multiplyer for PWM frequency
-		i2c->write_byte(MODE1, 0x80); //restart
-		i2c->write_byte(MODE2, 0x04); //totem pole (default)
+  for (int a=0; a< 9; a++)
+  {
+    wiringPiI2CWriteReg8(fd, 0x06+4*a, 0  &  0xFF);
+    wiringPiI2CWriteReg8(fd, 0x07+4*a, 0  >> 8);
+    wiringPiI2CWriteReg8(fd, 0x08+4*a,(150 + position[a]) &  0xFF);
+    wiringPiI2CWriteReg8(fd, 0x09+4*a,(150 + position[a]) >> 8);
+  //  cout << a<< endl;
+  }
 }
 
-//! PWM a single channel
-/*!
- \param led channel (1-16) to set PWM value for
- \param value 0-4095 value for PWM
- */
-void PCA9685::setPWM(uint8_t led, int value) {
-	setPWM(led, 0, value);
-}
-//! PWM a single channel with custom on time
-/*!
- \param led channel (1-16) to set PWM value for
- \param on_value 0-4095 value to turn on the pulse
- \param off_value 0-4095 value to turn off the pulse
- */
-void PCA9685::setPWM(uint8_t led, int on_value, int off_value) {
-		i2c->write_byte(LED0_ON_L + LED_MULTIPLYER * (led - 1), on_value & 0xFF);
-		i2c->write_byte(LED0_ON_H + LED_MULTIPLYER * (led - 1), on_value >> 8);
-		i2c->write_byte(LED0_OFF_L + LED_MULTIPLYER * (led - 1), off_value & 0xFF);
-		i2c->write_byte(LED0_OFF_H + LED_MULTIPLYER * (led - 1), off_value >> 8);
-}
+//Setting all servos to zero. Turning them all off. 
 
-//! Get current PWM value
-/*!
- \param led channel (1-16) to get PWM value from
- */
-int PCA9685::getPWM(uint8_t led){
-	int ledval = 0;
-	ledval = i2c->read_byte(LED0_OFF_H + LED_MULTIPLYER * (led-1));
-	ledval = ledval & 0xf;
-	ledval <<= 8;
-	ledval += i2c->read_byte(LED0_OFF_L + LED_MULTIPLYER * (led-1));
-	return ledval;
+
+void PCA9685::Turn_off(int fd)
+{
+  for (int a = 0; a<9; a++)
+  {
+    wiringPiI2CWriteReg8(fd, 0x06+4*a, 0 &  0xFF);
+    wiringPiI2CWriteReg8(fd, 0x07+4*a, 0 >> 8);
+    wiringPiI2CWriteReg8(fd, 0x08+4*a, 0 &  0xFF);
+    wiringPiI2CWriteReg8(fd, 0x09+4*a, 0 >> 8);
+
+  }
 }
--- main.cpp
+++ main.cpp
@@ -1,90 +1,79 @@
-#include <unistd.h>    //Needed for I2C port
-#include <fcntl.h>     //Needed for I2C port
-#include <sys/ioctl.h> //Needed for I2C port
-#include <linux/i2c-dev.h> //Needed for I2C port
+#include <string.h>
 #include <iostream>
-#include "PCA9685.h" //PCA9685
-#include <stdint.h>
-
+#include <wiringPiI2C.h>
+//#include <unistd.h>
+//#include <stdint.h>
+#include "PCA9685.h"
+#include <math.h>
 using namespace std;
 
-unsigned char buffer[60] = {0};
+int stand[9]={20,100,90,20,100,90,20,100,90};
+int value = 0;
+int forward[9];
+int fd = wiringPiI2CSetup(0x40);
+int speed = 1000000;
+PCA9685 PWM;
 
-    
-void Send(char add1,char  add2,int file_i2c)
-{
-  //----- write -----
 
-  buffer[0] = add1;
-  buffer[1] = add2;
-  int length = 2;                 //<<< Number of bytes to write
-//  cout<<"Buffer#1: "<<int(buffer[0])<<" Buffer#2: "<<int(buffer[1])<<endl;
-  if (write(file_i2c, buffer, length) != length)              //write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
-  {
-    /* ERROR HANDLING: i2c transaction failed */
-    cout<<"Failed to write to the i2c bus.\n";
-  }
-  //cout<<file_i2c<<endl;
+void walk()
+{ 
+  memcpy(forward, stand, sizeof(stand));
+  cout<< "Enter a number: " <<endl;
+  int user; 
+  cin >> user;
+  //1
+  //forward[2] = 115;
+  forward[1] = 150;
+  forward[7] = 100;
+  forward[4] = user;
+  PWM.Set_Pwm(fd, forward);
+  usleep(speed);
+  //2
+  //forward[2] = 140;
+  forward[1] = 100;
+  forward[7] = 100;
+  forward[4] = 100;
+  PWM.Set_Pwm(fd, forward);
+  usleep(speed);
+  //3
+  //forward[2] = 120;
+  PWM.Set_Pwm(fd, forward);
+  usleep(speed);
+  //4
+  //forward[2] = 75;
+  PWM.Set_Pwm(fd, forward);
+  usleep(speed);
+  //6
+  //forward[2] = 30;
+  forward[4] = 100;
+  forward[7] = 120;
+  PWM.Set_Pwm(fd, forward);
+  usleep(speed);
+  //7
+  //forward[2] = 75;
+  PWM.Set_Pwm(fd, forward);
+  usleep(speed);
 
 }
 
-void SetFreq(int freq,int file_i2c)
-{
-  uint8_t prescale_val = (CLOCK_FREQ / 4096 / freq)  - 1;
-  Send(MODE1,0x10,file_i2c); //sleep
-  Send(PRE_SCALE,prescale_val,file_i2c);
-  Send(MODE1,0x80,file_i2c);
-  Send(MODE2,0x04,file_i2c);
-  //cout<<file_i2c<<endl;
-}
-
-void SetPWM(uint8_t led, int on_value, int off_value,int file_i2c)
-{
-  cout<<"Start"<<endl;
-  Send(LED0_ON_L + LED_MULTIPLYER * (led - 1), on_value & 0xFF,file_i2c);
-  Send(LED0_ON_H + LED_MULTIPLYER * (led - 1), on_value >> 8,file_i2c);
-  Send(LED0_OFF_L + LED_MULTIPLYER * (led - 1), off_value & 0xFF,file_i2c);
-  Send(LED0_OFF_H + LED_MULTIPLYER * (led - 1), off_value >> 8,file_i2c);
-  //cout<<file_i2c<<endl;
-}
 
 
 int main()
 {
-  int file_i2c;
-  int length;
-
-  //----- OPEN THE I2C BUS -----
-  char *filename = (char*)"/dev/i2c-1";
-  if ((file_i2c = open(filename, O_RDWR)) <0)
+  //int fd;
+  //int stand[9]={20,120,90,20,120,90,20,120,90}; 
+  int value = 0;
+    
+  //fd = wiringPiI2CSetup(0x40);
+  //PCA9685 PWM;
+  PWM.Set_Freq(fd);
+  
+  while(value <=2)
   {
-    //ERROR HANDLING: you can check errno to see what went wrong
-    cout<<"Failed to open the i2c bus"<<endl;
+    PWM.Set_Pwm(fd, stand);
+    sleep(1);
+    walk();
+    value++;
   }
-
-  int addr = 0x40;          //<<<<<The I2C address of the slave
-
-  if (ioctl(file_i2c, I2C_SLAVE, addr) < 0)
-  {
-    cout<<"Failed to acquire bus access and/or talk to slave.\n";
-    //ERROR HANDLING; you can check errno to see what went wrong
-  }
-
-    //----Set mode to 0----//
-  Send(MODE1,0b00010000,file_i2c);
-  Send(MODE1,0b00000000,file_i2c);
-  usleep(5000);
-  Send(MODE2,0b00000100,file_i2c);
-
-
-  //----Set Freq to 60----//
-  SetFreq(60,file_i2c);
-
-  for(int a =0; a<=500; a++)
-  {
-    SetPWM(0,0,a,file_i2c);
-    usleep(10000);
-    cout<<150+a<<endl;
-  }
-
+  PWM.Turn_off(fd);
 }
